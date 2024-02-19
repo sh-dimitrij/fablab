93e0b0ad923ad83254348342cbaa2e9b88e22211
John Doe <johndoe@example.com>
2024-02-19 22:48:25 +0300

    git pwa

--- registerSW.js
+++
@@ -1,1 +0,0 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('https://github.com/sh-dimitrij/fablab/sw.js', { scope: 'https://github.com/sh-dimitrij/fablab/' })})}
